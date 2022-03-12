7f8fbcd5fa8f4f0cd1eff17ddf3f1bb0eafc2112
Afif Osama <afifosama66@gmail.com>
2022-03-12 15:43:29 +0700

    Added: main_page

---
+++ lib/app/utils/theme.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = Color(0xff3F1871);
+Color blackColor = Colors.black;
+
+double edge24 = 24;
+
+TextStyle title1 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w500, color: blackColor, fontSize: 28);
+
+TextStyle title2 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w500, color: blackColor, fontSize: 24);
+
+TextStyle title3 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w500, color: blackColor, fontSize: 20);
+
+TextStyle titleCard1 = GoogleFonts.poppins(
+    fontWeight: FontWeight.w300, color: blackColor, fontSize: 16);
+
+TextStyle finished = GoogleFonts.poppins(
+    fontWeight: FontWeight.w300, color: Colors.green, fontSize: 14);
+
+TextStyle unFinished = GoogleFonts.poppins(
+    fontWeight: FontWeight.w300, color: Colors.red, fontSize: 14);
