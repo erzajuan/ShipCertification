ad525ba54072ef5010ab04441ccacab5387675f8
erzajuan <direkal2@gmail.com>
2022-03-14 21:38:03 +0700

    Login Page

--- lib/app/modules/login_page/views/login_page_view.dart
+++ lib/app/modules/login_page/views/login_page_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/login_page_controller.dart';
 
 class LoginPageView extends GetView<LoginPageController> {
@@ -9,17 +7,128 @@ class LoginPageView extends GetView<LoginPageController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('LoginPageView'),
+        title: Text('Login'),
         centerTitle: true,
       ),
-      body: Center(
+      body: SingleChildScrollView(
+        child: Center(
           child: Column(
-        children: [
-          TextField(
-            controller: controller.usernameController,
-          )
-        ],
-      )),
+            children: [
+              SizedBox(
+                height: 173,
+              ),
+              Image.asset(
+                "assets/LogoDishub.png",
+                width: 286,
+                height: 328,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Text(
+                "PRO KSOP",
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                width: 312,
+                height: 45,
+                child: TextField(
+                  controller: controller.emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: false,
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    labelText: "E-Mail",
+                    labelStyle: TextStyle(
+                        color: Color(0xFF3F1871),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      borderSide: BorderSide(
+                        color: Color(0xFFFCDB00),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      borderSide: BorderSide(
+                        color: Color(0xFF3F1871),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              SizedBox(
+                width: 312,
+                height: 45,
+                child: Obx(
+                  () => TextField(
+                    controller: controller.passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: controller.secureText.isTrue,
+                    maxLines: 1,
+                    decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                            color: Color(0xFF3F1871),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          borderSide: BorderSide(
+                            color: Color(0xFF3F1871),
+                          ),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            controller.secureText.value
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                          onPressed: () {
+                            controller.changeVisibility();
+                          },
+                        )),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 37,
+              ),
+              SizedBox(
+                width: 312,
+                height: 47,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(18.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF3F1871),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Sign In",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      builder: ((context, snapshot) {
+        return GetMaterialApp(
+          title: "Ship Certification",
+          initialRoute: Routes.LOGIN_PAGE,
+          getPages: AppPages.routes,
+        );
+      }),
+    );
+  }
 }
